0e636c627d0f0fbffdd94a9e7901d82c62bfebb5
golo <a.golovatjuk@gmail.com>
2016-02-13 17:20:28 +0500

    pthread_rwlock ready

--- pthread_mutex.cpp
+++ pthread_mutex.cpp
@@ -32,19 +32,19 @@ PID запущенного процесса должен быть сохране
 using namespace std;
 int counter1 = 0;
 int counter2 = 0;
+int counter3 = 0;
 pthread_mutex_t mLock;
 pthread_spinlock_t sLock;
+pthread_rwlock_t   rwLock;
 
 void *helloMutex(void *args){
 
     pthread_mutex_lock(&mLock);
-
     counter1 +=1;    
     cout << "Under Mutex...start:" << counter1 << endl;
 //    for(int i=0; i<(0xFFFFFFFF);i++);
     sleep(5);
     cout << "Under Mutex...finished:" << counter1 << endl;
-
     pthread_mutex_unlock(&mLock);
     
     pthread_exit(0);
@@ -56,11 +56,34 @@ void *helloSpin(void *args){
 
     counter2 +=1;    
     cout << "Under SpinLock...start:" << counter2 << endl;
-//    for(int i=0; i<(0xFFFFFFFF);i++);
     sleep(2);
     cout << "Under SpinLock...finished:" << counter2 << endl;
-
     pthread_spin_unlock(&sLock);
+    
+    pthread_exit(0);
+}
+
+void *helloRead(void *args){
+
+    pthread_rwlock_rdlock(&rwLock);
+
+    cout << "Under ReadLock...start:" << counter3 << endl;
+    sleep(3);
+    cout << "Under ReadLock...finished:" << counter3 << endl;
+    pthread_rwlock_unlock(&rwLock);
+    
+    pthread_exit(0);
+}
+
+void *helloWrite(void *args){
+
+    pthread_rwlock_wrlock(&rwLock);
+
+    counter3 +=1;    
+    cout << "Under WriteLock...start:" << counter3 << endl;
+    sleep(5);
+    cout << "Under WriteLock...finished:" << counter3 << endl;
+    pthread_rwlock_unlock(&rwLock);
     
     pthread_exit(0);
 }
@@ -70,29 +93,45 @@ void *helloSpin(void *args){
  */
 int main(int argc, char** argv) {
 
+//    cout << "Mutex hello!" << endl;
+
     pthread_t thread1, thread2; // array of threads better
     pthread_t thread3, thread4; // array of threads better
-    int t1, t2, t3, t4, err1;
+    pthread_t thread5, thread6; // array of threads better
+    pthread_t thread7, thread8; // array of threads better
+    int t1, t2, t3, t4, t5, t6, t7, t8, err1;
     
     mLock = PTHREAD_MUTEX_INITIALIZER;
 //    err1 = pthread_mutex_init(&mLock, NULL);
     err1 = pthread_spin_init(&sLock, PTHREAD_PROCESS_PRIVATE);
-            
+   
+    rwLock = PTHREAD_RWLOCK_INITIALIZER;
+    
     t1 = pthread_create(&thread1, NULL, helloMutex, NULL);
     t2 = pthread_create(&thread2, NULL, helloMutex, NULL);
     
     t3 = pthread_create(&thread3, NULL, helloSpin, NULL);
     t4 = pthread_create(&thread4, NULL, helloSpin, NULL);
 
+    t5 = pthread_create(&thread5, NULL, helloRead, NULL);
+    t6 = pthread_create(&thread6, NULL, helloWrite, NULL);
+    t7 = pthread_create(&thread7, NULL, helloRead, NULL);
+    t8 = pthread_create(&thread8, NULL, helloRead, NULL);
+
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
     pthread_join(thread3, NULL);
     pthread_join(thread4, NULL);
-    
-    cout << "Mutex hello!" << endl;
+
+    pthread_join(thread5, NULL);
+    pthread_join(thread6, NULL);
+    pthread_join(thread7, NULL);
+    pthread_join(thread8, NULL);
+
     pthread_mutex_destroy(&mLock);
     pthread_spin_destroy(&sLock);
+    pthread_rwlock_destroy(&rwLock);
     
     return 0;
 }
